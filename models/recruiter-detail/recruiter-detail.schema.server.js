66c01a764a3ce2a0c0c07ded2e417f7f995ad222
rastogi-s <35542305+rastogi-s@users.noreply.github.com>
2018-06-24 17:34:54 -0400

    recruiter details changes

--- models/recruiter-detail/recruiter-detail.schema.server.js
+++ models/recruiter-detail/recruiter-detail.schema.server.js
@@ -1,7 +1,8 @@
 var mongoose = require('mongoose');
 var recruiterDetailSchema = mongoose.Schema({
     title : String,
-    company : {type: mongoose.Schema.Types.ObjectId, ref: 'CompanyModel'}
+    company: String,
+    // company : {type: mongoose.Schema.Types.ObjectId, ref: 'CompanyModel'}
     user : {type: mongoose.Schema.Types.ObjectId, ref: 'UserModel'}
 
 }, {collection: 'RecruiterDetail'});
